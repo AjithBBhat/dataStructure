2feef13a6b3f98d0c304d2e0352f7f095a23e296
AjithBBhat <ajithbbhat@gmail.com>
2024-12-16 23:26:24 +0530

    enhanced

--- go/search.go
+++ go/search.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+
+func contains(arr []int,element int)bool{
+	for _, val := range arr {
+        if val == element {
+            return true
+        }
+    }
+    return false  // element not found in the array
+}
 func main() {
 	arr := []int{1, 2, 3, 4}
-	element := 3
+	element := 5
 	found := false
 
 	for _, i := range arr {
@@ -18,4 +27,11 @@ func main() {
     } else {
 		fmt.Println(element," is not present in the array.\n",)
 	}
+
+	if contains(arr, element) {
+		fmt.Println(element," is present in the array.\n")
+    } else {
+        fmt.Println(element," is not present in the array.\n")
+	}
 }
+
--- js/search.js
+++ js/search.js
@@ -1,11 +1,29 @@
 //includes 
 
 let arr = [1,2,3,4,5,6,7,8,9,10,11,12]
-let element = 13
+let element = 3
 
 // Check if the array contains the element
 if (arr.includes(element)) {
     console.log(`${element} - exsits`)
+}else{
+    console.log(`${element} does not exist`)
+}
+
+//indexOf()
+
+console.log(arr.indexOf(element))
+if (arr.indexOf(element)!=-1) {
+    console.log(`${element} - exsits at index ${arr.indexOf(element)}`)
+}else{
+    console.log(`${element} does not exist`)
+}
+
+//find()
+
+let foundelement = arr.find(element => element ===3)
+if (foundelement) {
+    console.log(`${element} - exsits at index ${arr.indexOf(foundelement)}`)
 }else{
     console.log(`${element} does not exist`)
 }
