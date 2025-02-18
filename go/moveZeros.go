d3090489883581c2d77f641e6184cd9a80a14b64
AjithBBhat <ajithbbhat@gmail.com>
2025-02-19 00:24:19 +0530

    moveZeros

---
+++ go/moveZeros.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+//Brute force method time-O(n) space-O(n)
+
+// func moveZeroes(nums []int)  {
+// 	result := make([]int,0,len(nums))
+	
+// 	for _,num := range nums {
+// 		if num!=0 {
+// 			result = append(result,num)
+// 	}
+// }
+// 	for len(result)<len(nums) {
+// 		result = append(result,0)
+// 	}
+
+// 	copy(nums,result)
+// }
+
+//Two-Pass Approach with Extra Space  time-O(n) space-O(1)
+// func moveZeroes(nums []int) {
+// 	index :=0;
+
+// 	for _,num:= range nums {
+// 		if num !=0{
+// 			nums[index]=num
+// 			index++
+// 		}
+// 	}
+// 	for index<len(nums) {
+// 		nums[index]=0
+// 		index++
+// 	}
+// }
+//Two-Pointer Approach (Optimal) time-O(n) space-O(1)
+
+func moveZeroes(nums []int){
+	lastNonZeroFoundAt := 0;
+
+	for i:=0 ; i<len(nums) ; i++ {
+		if nums[i] !=0 {
+			nums[lastNonZeroFoundAt], nums[i] = nums[i],nums[lastNonZeroFoundAt]
+			lastNonZeroFoundAt++
+		}
+	}
+}
+
+
+func main() {
+	nums := []int {1,3,0,4,0}
+	moveZeroes(nums)
+	fmt.Println(nums) // Output: [1,3,4,0,0]
+
+}
---
+++ js/moveZero.js
@@ -0,0 +1,14 @@
+const array = [1,4,0,3,0];
+
+function moveZeros(array) {
+    let lastZeroFoundAt = 0
+    for( let i=0;i<array.length;i++ ){
+        if(array[i]!=0){
+            [array[lastZeroFoundAt],array[i]] = [array[i],array[lastZeroFoundAt]]
+            lastZeroFoundAt++
+        }
+    } 
+    return array;  // [1, 4, 3, 0, 0]  // [1, 4, 3, 0, 0] is the expected output. The zeros are moved to the end, and the order of the non-zero elements remains unchanged.
+}
+
+console.log(moveZeros(array))
